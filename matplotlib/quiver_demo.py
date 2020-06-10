abda181f96c349a61351cf874ac43b42b3100c02
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-06-10 18:09:18 +0200

    Add matplotlib example

---
+++ matplotlib/quiver_demo.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.set_xlim3d(0, 0.8)
+ax.set_ylim3d(0, 0.8)
+ax.set_zlim3d(0, 0.8)
+ax.quiver(0, 0, 0, 1, 1, 1, length = 0.5, normalize = False)
+plt.show()
