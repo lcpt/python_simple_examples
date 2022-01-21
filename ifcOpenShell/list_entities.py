b8d41a37e070db1807044ccb14f8a3e64a0631de
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-01-21 15:15:13 +0100

    Print beam details

--- ifcOpenShell/list_entities.py
+++ ifcOpenShell/list_entities.py
@@ -1,7 +1,17 @@
 import ifcopenshell
 import sys
 
-ifc_file = ifcopenshell.open(sys.argv[1])
+#inputFileName= sys.argv[1]
+inputFileName= './beams.ifc'
+
+ifc_file = ifcopenshell.open(inputFileName)
 products = ifc_file.by_type('IfcProduct')
+beams= list()
 for product in products:
     print(product.is_a())
+    if(product.is_a('IfcBeamStandardCase')):
+        beams.append(product)
+
+for b in beams:
+    print('beam Description:', b.Description)
+        
