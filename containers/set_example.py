17f110d515e43ff407208a0cc712a720169b1ef4
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-05-05 16:13:26 +0200

    Add container examples

---
+++ containers/set_example.py
@@ -0,0 +1,21 @@
+
+from __future__ import print_function
+
+# empty set
+set_1 = {}
+
+#set with single item
+set_2 = {54}
+set_2.add(7)
+
+#set with multiple items
+set_3 = {32, 41, 29, 85}
+
+#set with duplicate items
+set_4 = {32, 41, 29, 41, 32, 85}
+
+print(set_3)
+print(set_1)
+print(set_2)
+print(set_3)
+print(set_4)
