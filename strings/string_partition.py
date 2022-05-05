c7fab173466e5e8730eff885ed64562dfca5399e
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-05-05 10:05:38 +0200

    Add string manipulation example

---
+++ strings/string_partition.py
@@ -0,0 +1,10 @@
+
+from __future__ import print_function
+
+tim = '16:30:10'
+
+partition= tim.partition(':')
+fromColon= partition[2]
+
+print('partition: ', partition)
+print('from colon: ', fromColon)
