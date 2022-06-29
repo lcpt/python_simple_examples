e8818f8bb27ad71a15c6a2690ef06723e6205694
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-06-29 20:28:16 +0200

    Add full path to file name

--- exec_script/exec_example_04.py
+++ exec_script/exec_example_04.py
@@ -1,9 +1,11 @@
 import ast
+from pathlib import Path
 
 def inlineCode(inputFile):
+    fullPath= Path(inputFile).resolve()
     codeSource= open(inputFile).read()
     code= ast.parse(codeSource)
-    compiledCode= compile(code, filename=inputFile, mode='exec')
+    compiledCode= compile(code, filename=str(fullPath), mode='exec')
     return exec(compiledCode)
 
 inlineCode('./deliberate_error.py')
