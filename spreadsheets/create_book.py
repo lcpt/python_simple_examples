77c021c9b7c687d53cfab06e831348d33246c793
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2023-04-17 10:47:19 +0200

    Add spreadsheet example

---
+++ spreadsheets/create_book.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+''' Simple example that shows how to create an Excel book.'''
+
+from collections import OrderedDict
+import pyexcel as pe
+
+a_dictionary_of_two_dimensional_arrays = {
+         'Sheet 1':
+             [
+                 [1.0, 2.0, 3.0],
+                 [4.0, 5.0, 6.0],
+                 [7.0, 8.0, 9.0]
+             ],
+         'Sheet 2':
+             [
+                 ['X', 'Y', 'Z'],
+                 [1.0, 2.0, 3.0],
+                 [4.0, 5.0, 6.0]
+             ],
+         'Sheet 3':
+             [
+                 ['O', 'P', 'Q'],
+                 [3.0, 2.0, 1.0],
+                 [4.0, 3.0, 2.0]
+             ]
+     }
+data = OrderedDict()
+data.update({"Sheet 2": a_dictionary_of_two_dimensional_arrays['Sheet 2']})
+data.update({"Sheet 1": a_dictionary_of_two_dimensional_arrays['Sheet 1']})
+data.update({"Sheet 3": a_dictionary_of_two_dimensional_arrays['Sheet 3']})
+pe.save_book_as(bookdict=data, dest_file_name="book.ods")
