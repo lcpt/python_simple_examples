8bc1f1c80dfe48f80acad15df5da71170e0ac280
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2023-09-13 19:25:41 +0200

    The previous version was wrong

--- freecad/techdraw_view.py
+++ freecad/techdraw_view.py
@@ -9,28 +9,27 @@ cube= Part.show(cubeShape)
 cube.Label= 'cube'
 
 cone = doc.addObject("Part::Cone", "cone")
-cone.Radius1 = 5
+cone.Radius1 = 2.5
 cone.Radius2 = 0
 cone.Height = 3
 cone.Angle = 270
 cone.Placement = App.Placement(App.Vector(1, 1, 3), App.Rotation(0, 0, 0))
 
-box= doc.Shape
 page= doc.addObject('TechDraw::DrawPage','Page')
 template= doc.addObject('TechDraw::DrawSVGTemplate','Template')
-template.Template= App.ConfigGet('AppHomePath')+'Mod/Drawing/Templates/A1_Landscape_blank.svg'
+template.Template= App.ConfigGet('AppHomePath')+'Mod/TechDraw/Templates/A3_Landscape_blank.svg'
 page.Template= template
 
-view = activeDoc.addObject("TechDraw::DrawViewPart", "View")
+view = doc.addObject("TechDraw::DrawViewPart", "View")
 page.addView(view)
-view.Source = box
+view.Source = [cube, cone]
 view.Direction = (0, 0, 1)
+view.ScaleType= u'Custom'
+view.Scale= 25
+# view.Rotation='90.00 deg'
+# view.HardHidden= True
+view.Caption='Top view'
+view.recompute()
 
-section = doc.addObject("TechDraw::DrawViewSection", "Section")
-page.addView(section)
-section.Source = box
-section.BaseView = view
-section.Direction = (0, 1, 0)
-section.SectionNormal = (-1, 0, 0)
 
 doc.recompute()
