8dba462e6cd387b7c02b3e9ed72e271d4612fc50
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2023-09-13 15:37:08 +0200

    Add shape2dview example

---
+++ freecad/shape2dview_example.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import FreeCAD as App
+import Draft
+
+doc = App.newDocument()
+
+box = doc.addObject("Part::Box", "Box")
+box.Length = 2300
+box.Width = 500
+box.Height = 1000
+
+shape1 = Draft.make_shape2dview(box)
+
+shape2 = Draft.make_shape2dview(box, App.Vector(1, -1, 1))
+
+shape3 = Draft.make_shape2dview(box, App.Vector(-1, 1, 1), [0, 5])
+shape3.ProjectionMode = "Individual Faces"
+
+doc.recompute()
