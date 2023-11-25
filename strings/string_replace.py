14c7b711607223a3df19e6e5c322629f737ac1d1
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2023-11-25 12:48:40 +0100

    Add examples for replace upper and lower Python methods

---
+++ strings/string_replace.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+''' String replace demo.'''
+
+__author__= "Luis C. Pérez Tato (LCPT)"
+__copyright__= "Copyright 2023, LCPT"
+__license__= "GPL"
+__version__= "3.0"
+__email__= "l.pereztato@ciccp.es"
+
+message = 'Python is really useful'
+
+# convert message to lowercase
+msg= message.replace(' ','_')
+print(message)
+print(msg)
+
+
+# Output: python is fun
+
---
+++ strings/string_upper_lower.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+''' Uppercase and lowercase conversion demo.'''
+
+__author__= "Luis C. Pérez Tato (LCPT)"
+__copyright__= "Copyright 2023, LCPT"
+__license__= "GPL"
+__version__= "3.0"
+__email__= "l.pereztato@ciccp.es"
+
+message = 'Python is really useful'
+
+# convert message to lowercase
+msg= message.lower()
+MSG= message.upper()
+print(message)
+print(msg)
+print(MSG)
+
+
+# Output: python is fun
+
