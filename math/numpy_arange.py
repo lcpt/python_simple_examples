57b5bb14adf0d77e56b906a3e8d3b76a47340434
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2024-04-19 14:18:47 +0200

    Add numpy.arange example

---
+++ math/numpy_arange.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+''' Example of use of numpy.arange'''
+
+__author__= "Luis C. Pérez Tato (LCPT)"
+__copyright__= "Copyright 2024, LCPT"
+__license__= "GPL"
+__version__= "3.0"
+__email__= "l.pereztato@ciccp.es"
+
+import numpy
+
+test= numpy.arange(start= -1.0, stop= 1.0, step= 0.1)
+
+print(test)
