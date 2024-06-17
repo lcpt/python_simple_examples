e711c3aa09821c95f6413e869b95c0c07b9554b9
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2024-06-17 16:46:50 +0200

    Add vtkPoints data structure example

---
+++ graphics/vtk/vtk_points.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# noinspection PyUnresolvedReferences
+from vtk import vtkInteractionStyle
+# noinspection PyUnresolvedReferences
+import vtk.vtkRenderingOpenGL2
+from vtk.vtkCommonColor import vtkNamedColors
+from vtk.vtkCommonCore import vtkPoints
+from vtk.vtkCommonDataModel import (
+    vtkCellArray,
+    vtkPolyData
+)
+from vtk.vtkRenderingCore import (
+    vtkActor,
+    vtkPolyDataMapper,
+    vtkRenderWindow,
+    vtkRenderWindowInteractor,
+    vtkRenderer
+)
+
+
+def main():
+    colors = vtkNamedColors()
+
+    # Create the geometry of a point (the coordinate)
+    points = vtkPoints()
+    p = [1.0, 2.0, 3.0]
+
+    # Create the topology of the point (a vertex)
+    vertices = vtkCellArray()
+    # We need an an array of point id's for InsertNextCell.
+    pid = [0]
+    pid[0] = points.InsertNextPoint(p)
+    vertices.InsertNextCell(1, pid)
+
+    # Create a polydata object
+    point = vtkPolyData()
+
+    # Set the points and vertices we created as the geometry and topology of the polydata
+    point.SetPoints(points)
+    point.SetVerts(vertices)
+
+    # Visualize
+    mapper = vtkPolyDataMapper()
+    mapper.SetInputData(point)
+
+    actor = vtkActor()
+    actor.SetMapper(mapper)
+    actor.GetProperty().SetColor(colors.GetColor3d('Tomato'))
+    actor.GetProperty().SetPointSize(20)
+
+    renderer = vtkRenderer()
+    renderWindow = vtkRenderWindow()
+    renderWindow.SetWindowName('Point')
+    renderWindow.AddRenderer(renderer)
+    renderWindowInteractor = vtkRenderWindowInteractor()
+    renderWindowInteractor.SetRenderWindow(renderWindow)
+
+    renderer.AddActor(actor)
+    renderer.SetBackground(colors.GetColor3d('DarkGreen'))
+
+    renderWindow.Render()
+    renderWindowInteractor.Start()
+
+
+if __name__ == '__main__':
+    main()
