34c6ca22f92cc6145cd47cce20aeaf26c2ecc2df
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2024-07-08 18:54:52 +0200

    Update code
    MatplotlibDeprecationWarning: Calling gca() with keyword arguments
    was deprecated in Matplotlib 3.4. Starting two minor releases later,
    gca() will take no keyword arguments. The gca() function should only
    be used to get the current axes, or if no axes exist, create new
    axes with default keyword arguments. To create a new axes with
    non-default arguments, use plt.axes() or plt.subplot().

--- graphics/matplotlib/quiver_demo.py
+++ graphics/matplotlib/quiver_demo.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+ax = fig.add_subplot(projection='3d')
 ax.set_xlim3d(0, 0.8)
 ax.set_ylim3d(0, 0.8)
 ax.set_zlim3d(0, 0.8)
