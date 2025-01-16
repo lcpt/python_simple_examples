849db17dcedf574343e60a678da81ff33b6493be
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2025-01-16 19:36:59 +0100

    Set line width
    To illustrate how it is done.

--- graphics/vtk/vtk_polyline.py
+++ graphics/vtk/vtk_polyline.py
@@ -63,6 +63,7 @@ def main():
 
     actor = vtkActor()
     actor.SetMapper(mapper)
+    actor.GetProperty().SetLineWidth(5)
     actor.GetProperty().SetColor(colors.GetColor3d('Tomato'))
 
     # Setup render window, renderer, and interactor
