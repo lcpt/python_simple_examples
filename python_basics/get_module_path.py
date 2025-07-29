f11e4a8fe52343e10974f73a3a556dc027b5b43e
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2025-07-29 15:03:21 +0200

    Code cosmetics

--- python_basics/get_module_path.py
+++ python_basics/get_module_path.py
@@ -4,12 +4,12 @@ import os
 os_path =inspect.getfile(os)
 os_dirname= os.path.dirname(os_path)
 
-this_path= inspect.getfile(inspect.currentframe())
-this_dirname= os.path.dirname(this_path)
+this_file_path= inspect.getfile(inspect.currentframe())
+this_dirname= os.path.dirname(this_file_path)
 
 print(os_path)
 print(os_dirname)
 
-print(this_path)
+print(this_file_path)
 print(this_dirname)
 
